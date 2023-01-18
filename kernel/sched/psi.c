81e63b3407714421b6cc72fcf9cf32529c188e8f
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2023-01-18 09:41:02 +0000

    fix olog errors

--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -141,7 +141,6 @@
 #include <linux/file.h>
 #include <linux/poll.h>
 #include <linux/psi.h>
-#include <linux/ologk.h>
 #include "sched.h"
 
 static int psi_bug __read_mostly;
@@ -423,16 +422,6 @@ static u64 update_averages(struct psi_group *group, u64 now)
 
 			total_some = div_u64(group->total[PSI_AVGS][some], NSEC_PER_USEC);
 			total_full = div_u64(group->total[PSI_AVGS][full], NSEC_PER_USEC);
-
-			perflog(PERFLOG_UNKNOWN, "[PSI][%s][%s] avg10=[%lu.%02lu/%lu.%02lu]  avg60=[%lu.%02lu/%lu.%02lu]  avg300=[%lu.%02lu/%lu.%02lu] total=[%llu/%llu]",
-				   strtitle, (group == &psi_system) ? "SYSTEM" : "CGROUP",
-				   LOAD_INT(group->avg[some][AVG10]), LOAD_FRAC(group->avg[some][AVG10]),
-				   LOAD_INT(group->avg[full][AVG10]), LOAD_FRAC(group->avg[full][AVG10]),
-				   LOAD_INT(group->avg[some][AVG60]), LOAD_FRAC(group->avg[some][AVG60]),
-				   LOAD_INT(group->avg[full][AVG60]), LOAD_FRAC(group->avg[full][AVG60]),
-				   LOAD_INT(group->avg[some][AVG300]), LOAD_FRAC(group->avg[some][AVG300]),
-				   LOAD_INT(group->avg[full][AVG300]), LOAD_FRAC(group->avg[full][AVG300]),
-				   total_some, total_full);
 		}
 	}
 
