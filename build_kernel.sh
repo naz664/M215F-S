d1646980aedab0bd3add2ec1b21b2baf9c7cd00f
ghazzor <kartikhis8o@gmail.com>
2023-10-16 21:37:33 +0530

    modified:   arch/arm64/configs/exynos9610-m21dd_defconfig 	modified:   build_kernel.sh 	modified:   drivers/Kconfig 	modified:   drivers/Makefile 	deleted:    drivers/security/samsung/five_tee_driver/Kconfig 	deleted:    drivers/security/samsung/five_tee_driver/Makefile 	deleted:    drivers/security/samsung/five_tee_driver/five_ta_uuid.h 	deleted:    drivers/security/samsung/five_tee_driver/five_tee_driver.c 	deleted:    drivers/security/samsung/five_tee_driver/five_tee_interface.h 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/include/gp-api/tee_client_api.h 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/tee_client_api.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_common.h 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_common_emulator.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_common_qsee.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_common_tbase.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_common_teegris_v2.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_operation.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_operation.h 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/client/teec_operation_teegris_v3.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/protocol/protocol.h 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/protocol/socket_utils.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/protocol/socket_utils.h 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/protocol/teec_param_utils.c 	deleted:    drivers/security/samsung/five_tee_driver/multibuild/source/gp-api/protocol/teec_param_utils.h 	deleted:    drivers/security/samsung/icdrv/Kconfig 	deleted:    drivers/security/samsung/icdrv/Makefile 	deleted:    drivers/security/samsung/icdrv/icd.c 	deleted:    drivers/security/samsung/icdrv/icd.h 	deleted:    drivers/security/samsung/icdrv/icd_protect_list.gen.h 	deleted:    drivers/security/samsung/icdrv/icd_protect_list.h 	deleted:    drivers/security/samsung/icdrv/kinibi_atf_oemflag.c 	deleted:    drivers/security/samsung/icdrv/kinibi_oemflag.c 	deleted:    drivers/security/samsung/icdrv/oemflag.c 	deleted:    drivers/security/samsung/icdrv/oemflag.h 	deleted:    drivers/security/samsung/icdrv/oemflag_arch.h 	deleted:    drivers/security/samsung/icdrv/qsee_oemflag.c 	deleted:    drivers/security/samsung/icdrv/test/Makefile 	deleted:    drivers/security/samsung/icdrv/test/icdrv_kunit_test.c 	deleted:    drivers/security/samsung/icdrv/tzdev_oemflag.c 	deleted:    drivers/security/samsung/tzic/Kconfig 	deleted:    drivers/security/samsung/tzic/Makefile 	deleted:    drivers/security/samsung/tzic/tzic.h 	deleted:    drivers/security/samsung/tzic/tzic64.c 	deleted:    drivers/security/samsung/tzic/tzic_default.c 	deleted:    drivers/security/samsung/tzic/tzic_kinibi.c 	deleted:    drivers/security/samsung/tzic/tzic_mtk_atf.c 	deleted:    drivers/security/samsung/tzic/tzic_qsee.c 	deleted:    drivers/security/samsung/tzic/tzic_tzdev.c 	modified:   security/Kconfig 	modified:   security/Makefile

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+make clean && make distclean
+export CROSS_COMPILE=toolchain/gcc/bin/aarch64-linux-android-
+export LD=toolchain/clang/host/linux-86/clang-4639204/bin/lld
+export CC=toolchain/clang/host/linux-86/clang-4639204/bin/clang
+export CLANG_TRIPLE=toolchain/clang/host/linux-86/clang-4639204/bin/aarch64-linux-gnu-
+export AR=toolchain/clang/host/linux-86/clang-4639204/bin/clang/llvm-ar
+export NM=toolchain/clang/host/linux-86/clang-4639204/bin/clang/llvm-nm
+export OBJCOPY=toolchain/clang/host/linux-86/clang-4639204/bin/clang/llvm-objcopy
+export OBJDUMP=toolchain/clang/host/linux-86/clang-4639204/bin/clang/llvm-objdump
+export STRIP=toolchain/clang/host/linux-86/clang-4639204/bin/clang/llvm-strip
 
 export ARCH=arm64
 export PLATFORM_VERSION=12
