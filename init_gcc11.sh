e00c3e3f1f1ad7ed2d14cb69ae4e50ab8762172b
ghazzor <127341071+ghazzor@users.noreply.github.com>
2023-11-20 02:35:40 +0530

    add workflow hat compiles with gcc 11.3

---
+++ init_gcc11.sh
@@ -0,0 +1,10 @@
+#this is to automate toolchain cloning cuz i am too lazy to clone stuff on different vm(s) againg and again , also to reduce commands in workflow
+
+#clone proton clang v13 toolchain
+git clone --depth=1 https://github.com/kdrag0n/proton-clang toolchain/proton-clang
+
+#gcc 11.3 
+wget https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz?rev=8d05006a68d24d929d602804ec9abfb4&hash=A09C3DEEEC9525EE0513C0F6E519D0F6
+tar -xf arm*.tar.xz
+mv  arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu gcc
+mv gcc toolchain
