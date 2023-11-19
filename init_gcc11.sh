9c8d16c2c985f2658fd332ab006a617cf26a9334
ghazzor <127341071+ghazzor@users.noreply.github.com>
2023-11-20 02:49:21 +0530

    Update init_gcc11.sh

--- init_gcc11.sh
+++ init_gcc11.sh
@@ -4,7 +4,8 @@
 git clone --depth=1 https://github.com/kdrag0n/proton-clang toolchain/proton-clang
 
 #gcc 11.3 
-wget https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz?rev=8d05006a68d24d929d602804ec9abfb4&hash=A09C3DEEEC9525EE0513C0F6E519D0F6
-tar -xf arm*.tar.xz
-mv  arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu gcc
+#wget https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz?rev=8d05006a68d24d929d602804ec9abfb4&hash=A09C3DEEEC9525EE0513C0F6E519D0F6
+wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+tar -xf arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+mv arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu gcc
 mv gcc toolchain
